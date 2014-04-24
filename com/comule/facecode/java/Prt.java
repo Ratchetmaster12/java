3150be0b3ba35a45eb81b4fe0ce656b734a832c0
Ratchetmaster12 <sebrox1457@gmail.com>
2014-04-24 13:18:57 +0100

    Created File need for Main1 to work

---
+++ com/comule/facecode/java/Prt.java
@@ -0,0 +1,19 @@
+//This file is for easier text printing
+package com.comule.facecode.java;
+
+/**
+* The basic printing library
+* @author Sebastian Gluch
+*/
+public class Prt{
+
+  /**
+   * Basic String printing functionallity
+   * @param s The String to print
+   */
+  public void print(String s){
+    //Long version of an casual print
+    System.out.print(s);
+  }
+
+}
