5fe4426cda009b680a2512b52eb4182215a08b29
Ratchetmaster12 <sebrox1457@gmail.com>
2014-04-24 13:15:40 +0100

    Created new Java file and added basic code to it

---
+++ com/comule/facecode/java/Main1.java
@@ -0,0 +1,13 @@
+//Example JAVA file
+package com.comule.facecode.java; //Declares folder position
+
+//Declares that this file is called Main1 and is of java type class
+public class Main1{
+    
+    //Main function, entry point of a single-threaded program
+    //String[] is and array of Strings (or lines of characters) this is for class arguments
+    public void main(String[] args){
+        //This function is from the Prt java file, for showing text in command prompt
+        print("Hey");   
+    }
+}
